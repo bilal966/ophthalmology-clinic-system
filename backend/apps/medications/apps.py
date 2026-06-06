f9be09726179b758f3094a93e47aa60c6dac293c
Muhammad Bilal Ilyas <bilal.hobnail@gmail.com>
2026-06-06 18:18:18 +0100

    Add medications app config

---
+++ backend/apps/medications/apps.py
@@ -0,0 +1,10 @@
+"""
+Medications app config.
+"""
+from django.apps import AppConfig
+
+
+class MedicationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.medications'
+    verbose_name = 'Medications'
